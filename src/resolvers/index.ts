4be793baf69bdb816ec26e1fb50b08b45ef5f74c
Shashwat Singh <shashwat123student@gmail.com>
2024-02-03 23:35:12 +0530

    Successfully setup graphql code generator

---
+++ src/resolvers/index.ts
@@ -0,0 +1,8 @@
+// import { authorResolvers } from './getData.resolver';
+// import { ApolloServer } from '@apollo/server';
+// import { startStandaloneServer } from '@apollo/server/standalone';
+// export const resolvers: Resolvers = {
+//     Book: bookResolvers,
+// };
+
+
